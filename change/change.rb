51f8c2e67433400b8bb6b62941fa43736f9c563e
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 11:51:11 -0300

    Coin Change solution formatting 3

--- change/change.rb
+++ change/change.rb
@@ -5,12 +5,15 @@ module Change
   # @param [Array] coins
   # @param [Integer] target
   def self.generate(coins, target)
+    candidate = []
+
     return [] if target.zero?
     raise NegativeTargetError.new 'Negative amount for change' if target < 0
     raise ImpossibleCombinationError.new 'Target value is wrong' if target < coins.min
 
     (1..((target / coins.min) + 1)).each do |i|
       coins.sort!.repeated_combination(i).each do |candidate|
+        # I will not delay the short-circuit just to use `find` ("""better""" style) instead of `each`
         return candidate if candidate.sum == target
       end
     end
