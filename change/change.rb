54dbc7d464e883a2b350251043bf5fa59ae4a16c
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 11:07:46 -0300

    Coin Change solution formatting 2

--- change/change.rb
+++ change/change.rb
@@ -10,7 +10,6 @@ module Change
     raise ImpossibleCombinationError.new 'Target value is wrong' if target < coins.min
 
     (1..((target / coins.min) + 1)).each do |i|
-      # TODO: repeated_combination doesnt guarantee order, but will use anyway
       coins.sort!.repeated_combination(i).each do |candidate|
         return candidate if candidate.sum == target
       end
