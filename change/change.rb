2b80c96acfbebbf014716726f468300d705b6794
Rodrigo Dlugokenski <rdlu.one@gmail.com>
2020-09-23 10:51:00 -0300

    Coin Change solution

--- change/change.rb
+++ change/change.rb
@@ -1,8 +1,24 @@
-=begin
-Write your code for the 'Change' exercise in this file. Make the tests in
-`change_test.rb` pass.
+module Change
+  class ImpossibleCombinationError < StandardError; end
+  class NegativeTargetError < StandardError; end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/change` directory.
-=end
+  # @param [Array] coins
+  # @param [Integer] target
+  def self.generate(coins, target)
+    return [] if target.zero?
+    raise NegativeTargetError.new 'Negative amount for change' if target < 0
+    raise ImpossibleCombinationError.new 'Target value is wrong' if target < coins.min
+
+    (1..((target / coins.min) + 1)).each do |i|
+      # TODO: repeated_combination doesnt guarantee order, but will use anyway
+      coins.sort!.repeated_combination(i).each do |candidate|
+        if candidate.sum == target
+          return candidate
+        end
+      end
+    end
+
+    raise ImpossibleCombinationError.new('No change found')
+  end
+end
 
