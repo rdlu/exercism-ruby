79e02fb4f81689705ef2a658bb44b83792a7c2fe
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-10-27 16:15:24 -0300

    BST implementation (rescue)

---
+++ binary-search-tree/binary_search_tree.rb
@@ -0,0 +1,40 @@
+class Bst
+  attr_reader :data, :left, :right
+
+  # @param [Numeric] root
+  def initialize(root)
+    @data = root
+    @left = nil
+    @right = nil
+  end
+
+  # @param [Numeric] new_data
+  def insert(new_data)
+    if new_data <= data
+      push_left(new_data)
+    else
+      push_right(new_data)
+    end
+  end
+
+  def each(&block)
+    return enum_for(:each) unless block_given?
+    left.each(&block) if left
+    block.call(data)
+    right.each(&block) if right
+  end
+
+  private
+
+  def push_left(new_data)
+    left.insert(new_data)
+  rescue NoMethodError
+    @left = Bst.new(new_data)
+  end
+
+  def push_right(new_data)
+    right.insert(new_data)
+  rescue NoMethodError
+    @right = Bst.new(new_data)
+  end
+end
