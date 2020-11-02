067c3b26e3026693c6b7b3a63d609ea1a7bd6380
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-11-02 18:00:26 -0300

    Palindrome implementation

--- palindrome-products/palindrome_products.rb
+++ palindrome-products/palindrome_products.rb
@@ -1,8 +1,36 @@
-=begin
-Write your code for the 'Palindrome Products' exercise in this file. Make the tests in
-`palindrome_products_test.rb` pass.
+class Palindromes
+  Palindrome = Struct.new(:value, :factors)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/palindrome-products` directory.
-=end
+  def initialize(max_factor:, min_factor: 1)
+    @max, @min = max_factor, min_factor
+  end
 
+  def generate
+    candidates.each do |pair|
+      product = pair.reduce(:*)
+      palindromes[product] << pair if palindrome? product
+    end
+  end
+
+  def largest
+    Palindrome.new(*palindromes.max)
+  end
+
+  def smallest
+    Palindrome.new(*palindromes.min)
+  end
+
+  private
+
+  def candidates
+    (@min..@max).to_a.repeated_combination(2).lazy
+  end
+
+  def palindrome?(number)
+    number.to_s == number.to_s.reverse
+  end
+
+  def palindromes
+    @palindromes ||= Hash.new { |hash, key| hash[key] = [] }
+  end
+end
