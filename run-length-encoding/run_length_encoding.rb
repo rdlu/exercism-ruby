d043daa832a651d99b968637fb7fe2c55f6533e8
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-11-02 11:53:46 -0300

    RLE documentation

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -1,10 +1,16 @@
+# Run Length Encoding utils
 module RunLengthEncoding
 
+  # Encodes a string using RLE
   # @param [String] data
+  # @return [String]
   def self.encode(data)
     data.gsub(/(.)\1+/) { |match| "#{match.size}#{match[0]}" }
   end
 
+  # Decodes a string using RLE
+  # @param [String] data
+  # @return [String]
   def self.decode(data)
     data.gsub(/(\d+)([^0-9])/) { |_m| $2 * $1.to_i }
   end
