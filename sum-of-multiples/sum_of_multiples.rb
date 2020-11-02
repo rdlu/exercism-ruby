dc35e3de8fbf73ecf91278415ee532eb00e4d30e
Rodrigo Dlugokenski <rddweb@gmail.com>
2020-11-02 11:15:00 -0300

    Sum of Multiples: Early rejection

--- sum-of-multiples/sum_of_multiples.rb
+++ sum-of-multiples/sum_of_multiples.rb
@@ -2,12 +2,12 @@ class SumOfMultiples
 
   # @param [Array[Numeric]] factors
   def initialize(*factors)
-    @factors = factors
+    @factors = factors.reject(&:zero?)
   end
 
   # @param [Numeric] limit
   # @return [Numeric]
   def to(limit)
-    @factors.reject(&:zero?).flat_map { |m| (m...limit).step(m).to_a }.uniq.sum
+    @factors.flat_map { |m| (m...limit).step(m).to_a }.uniq.sum
   end
 end
